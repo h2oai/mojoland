b99b33e647b7d254ed9338ff0ebf38b8ee031e19
Pasha Stetsenko <pasha@h2o.ai>
2016-11-15 12:34:29 -0800

    Better javadocs for MojoModel

--- mojo-java/src/main/java/ai/h2o/mojos/runtime/MojoModel.java
+++ mojo-java/src/main/java/ai/h2o/mojos/runtime/MojoModel.java
@@ -1,9 +1,6 @@
 package ai.h2o.mojos.runtime;
 
-import ai.h2o.mojos.runtime.readers.FolderMojoReaderBackend;
-import ai.h2o.mojos.runtime.readers.MojoReader;
-import ai.h2o.mojos.runtime.readers.MojoReaderBackend;
-import ai.h2o.mojos.runtime.readers.ZipfileMojoReaderBackend;
+import ai.h2o.mojos.runtime.readers.*;
 
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -18,29 +15,40 @@ public abstract class MojoModel {
   /**
    * Primary factory method for constructing MojoModel instances.
    *
-   * @param file Name of the .mojo file (or folder) with the model's data. This should be the data retrieved via
-   *             the `GET /3/Models/{model_id}/mojo` endpoint.
-   * @return New `MojoModel` object.
-   * @throws IOException if `file` does not exist, or cannot be read, or does not represent a valid model.
+   * @param file Name of the model's .mojo file. This file can be obtained
+   *             either through the <code>GET /3/Models/{model_id}/mojo</code>
+   *             REST API endpoint, or <code>model.download_mojo()</code>
+   *             method call in Python/R clients.
+   *
+   * @return New <code>MojoModel</code> object.
+   * @throws IOException if <code>file</code> does not exist, or cannot be
+   *                     read, or does not represent a valid model.
    */
-  @SuppressWarnings("unused")  // Called by external users of the class
+  @SuppressWarnings("unused")
   public static MojoModel load(String file) throws IOException {
     File f = new File(file);
-    if (!f.exists())
+    if (!f.exists() || f.isDirectory())
       throw new FileNotFoundException("File " + file + " cannot be found.");
-    MojoReaderBackend cr = f.isDirectory()? new FolderMojoReaderBackend(file)
-        : new ZipfileMojoReaderBackend(file);
+    MojoReaderBackend cr = new ZipfileMojoReaderBackend(file);
     return MojoReader.readFrom(cr);
   }
 
+  
   /**
-   * Advanced way of constructing Mojo models by supplying a custom mojoReader.
+   * Advanced way of constructing Mojo models by supplying a custom
+   * mojo reader backend. For example a {@link FolderMojoReaderBackend} can be
+   * used to read an unzipped mojo model from a folder. Alternatively, you can
+   * implement your own backend, for example to read models stored on network
+   * locations, or load them from a database, etc.
    *
-   * @param mojoReader a class that implements the {@link MojoReaderBackend} interface.
-   * @return New `MojoModel` object
-   * @throws IOException if the mojoReader does
+   * @param mojoReader a class implementing the {@link MojoReaderBackend}
+   *                   interface.
+   *
+   * @return New <code>MojoModel</code> object.
+   * @throws IOException if mojo reader fails to read the model, or if the
+   *                     model retrieved is not valid.
    */
-  @SuppressWarnings("unused")  // Called by external users of the class
+  @SuppressWarnings("unused")
   public static MojoModel load(MojoReaderBackend mojoReader) throws IOException {
     return MojoReader.readFrom(mojoReader);
   }
