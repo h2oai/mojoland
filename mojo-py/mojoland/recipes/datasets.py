4ee98d3327a3155ce82f1674a911ce55ec69508d
Pasha Stetsenko <pasha@h2o.ai>
2016-11-16 23:25:37 -0800

    Add stars to the datasets.py module

--- mojo-py/mojoland/recipes/datasets.py
+++ mojo-py/mojoland/recipes/datasets.py
@@ -3,18 +3,38 @@ import csv
 import os
 import h2o
 
-__all__ = ("iris_frame", "iris_data")
+__all__ = ("iris_frame", "iris_data", "stars_frame", "stars_data")
 
+
+#---- Iris -------------------------------------------------------------------------------------------------------------
 
 def iris_frame():
     frame = h2o.upload_file(_file("iris.csv"))
-    assert frame.names == ["Sepal.Length", "Sepal.Width", "Petal.Length",
-                           "Petal.Width", "Species"]
+    assert frame.names == ["Sepal.Length", "Sepal.Width", "Petal.Length", "Petal.Width", "Species"]
     return frame
 
 
 def iris_data():
     with open(_file("iris.csv"), "r") as csvfile:
+        next(csvfile)  # skip the header
+        reader = csv.reader(csvfile, delimiter=",")
+        yield from reader
+
+
+#---- Stars ------------------------------------------------------------------------------------------------------------
+
+def stars_frame():
+    frame = h2o.upload_file(_file("stars.csv"), col_types={"name2": "string"})
+    assert frame.shape == (300, 12)
+    assert frame.names == ["name1", "name2", "coordL", "coordB", "spectral_type", "vis_mag", "vmc", "abs_mag", "amc",
+                           "prllx", "error", "distance"]
+    assert frame["spectral_type"].nlevels() == 175
+    assert frame.type("name1") == "string" and frame.type("name2") == "string"
+    return frame
+
+
+def stars_data():
+    with open(_file("stars.csv"), "r") as csvfile:
         next(csvfile)  # skip the header
         reader = csv.reader(csvfile, delimiter=",")
         yield from reader
