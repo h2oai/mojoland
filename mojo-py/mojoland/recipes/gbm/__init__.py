fdf65fdab23b612696219296a1d37d914fe8659a
Pasha Stetsenko <pasha@h2o.ai>
2016-11-18 16:00:41 -0800

    Enable StarsGbmRecipe

--- mojo-py/mojoland/recipes/gbm/__init__.py
+++ mojo-py/mojoland/recipes/gbm/__init__.py
@@ -2,5 +2,6 @@
 # -*- encoding: utf-8 -*-
 
 from .iris_gbm_recipe import IrisGbmRecipe
+from .stars_gbm_recipe import StarsGbmRecipe
 
-__all__ = ("IrisGbmRecipe", )
+__all__ = ("IrisGbmRecipe", "StarsGbmRecipe")
