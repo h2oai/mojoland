70054a1874922a927c8e265329c0c3a23b56f517
Pasha Stetsenko <pasha@h2o.ai>
2016-11-21 15:05:50 -0800

    Add a docstring

--- mojo-py/in_mojo_veritas.py
+++ mojo-py/in_mojo_veritas.py
@@ -1,8 +1,17 @@
 #!/usr/bin/env python3
 # -*- encoding: utf-8 -*-
+"""
+Primary script for running the mojo stability test harness.
+
+Running this script will go through all the recipes, and then for each recipe
+either create the latest mojo model if it doesn't exist yet, or load the model
+from the saved mojo file. Then it will proceed to testing all "nibbles" of the
+model against the saved results if they exist, and if they don't they will be
+created and saved too.
+"""
 import sys
 import mojoland
-assert sys.version_info[0] == 3, "Python3 is required"
+assert sys.version_info[0] == 3, "Python 3 is required"
 
 if __name__ == "__main__":
     cnsr = mojoland.Connoisseur()
